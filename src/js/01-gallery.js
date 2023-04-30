81ccd8c4f96c14fbd3fef06f80b58fc411a33c67
Sergiyan92 <sergiyan@ukr.net>
2023-04-30 14:42:09 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const createElementsGallery = galleryItems
+  .map(
+    item =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`
+  )
+  .join('');
 
+gallery.innerHTML = createElementsGallery;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+});
 console.log(galleryItems);
