c568176498d5952c99606337894a3cd93e50d9db
Sergiyan92 <sergiyan@ukr.net>
2023-04-30 16:26:36 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// Отримуємо посилання на елемент iframe та ініціалізуємо плеєр
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (event) {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
