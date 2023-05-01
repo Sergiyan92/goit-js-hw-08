2e00e1dfdd1e45b709b5a15eb809d8d37d4a4a64
Sergiyan92 <sergiyan@ukr.net>
2023-05-01 19:20:11 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,7 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
 const gallery = document.querySelector('.gallery');
 const createElementsGallery = galleryItems
   .map(
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
 
+const inputEmail = document.querySelector('[name="email"]');
+const inputMessage = document.querySelector('[name="message"]');
+const form = document.querySelector('.feedback-form');
+
+inputEmail.addEventListener(
+  'input',
+  throttle(() => {
+    const feedbackFormState = {
+      email: inputEmail.value,
+      message: inputMessage.value,
+    };
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify(feedbackFormState)
+    );
+  }, 500)
+);
+inputMessage.addEventListener(
+  'input',
+  throttle(() => {
+    const feedbackFormState = {
+      email: inputEmail.value,
+      message: inputMessage.value,
+    };
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify(feedbackFormState)
+    );
+  }, 500)
+);
+
+const formDefault =
+  JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+if (formDefault) {
+  inputEmail.value = formDefault.email || '';
+  inputMessage.value = formDefault.message || '';
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const feedbackFormState = {
+    email: inputEmail.value,
+    message: inputMessage.value,
+  };
+  console.log(feedbackFormState);
+  localStorage.removeItem('feedback-form-state');
+  inputEmail.value = '';
+  inputMessage.value = '';
+});
